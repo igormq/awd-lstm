3576b074551540629d061d6e051239490d553d46
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2020-11-25 20:24:25 -0300

    change config to search using the entire datraset

--- metrics.py
+++ metrics.py
@@ -1,17 +1,33 @@
 import torch
-from pytorch_lightning.metrics import TensorMetric
+from pytorch_lightning.metrics import Metric
 import torch.nn.functional as F
 import math
 
 
-class PPL(TensorMetric):
-    def __init__(self):
-        super().__init__(name='ppl')
+class PPL(Metric):
+    def __init__(self, dist_sync_on_step=False):
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+
+        self.add_state("correct",
+                       default=torch.tensor(0),
+                       dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor):
+        preds, target = self._input_format(preds, target)
+        assert preds.shape == target.shape
+
+        self.correct += torch.sum(preds == target)
+        self.total += target.numel()
 
     def forward(self, x, y=None):
         if y is None:
             return torch.exp(torch.as_tensor(x))
-        return F.cross_entropy(x, y, weight=None, ignore_index=-100, reduce=None, reduction='mean').exp()
+        return F.cross_entropy(x,
+                               y,
+                               weight=None,
+                               ignore_index=-100,
+                               reduce=None,
+                               reduction='mean').exp()
 
 
 class BPC(TensorMetric):
@@ -21,4 +37,9 @@ class BPC(TensorMetric):
     def forward(self, x, y=None):
         if y is None:
             return torch.as_tensor(x) / math.log(2)
-        return F.cross_entropy(x, y, weight=None, ignore_index=-100, reduce=None, reduction='mean') / math.log(2)
+        return F.cross_entropy(x,
+                               y,
+                               weight=None,
+                               ignore_index=-100,
+                               reduce=None,
+                               reduction='mean') / math.log(2)
